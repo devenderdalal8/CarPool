43ef5628819ed25e162305f5b8f8803f9d332ad0
Devender Dalal <devenderdalal8@gmail.com>
2026-02-12 15:22:54 +0530

    rename docker-compose file, add Dockerfile, and reduce JWT expiration times

--- src/main/java/com/carpool/carpool/util/JwtUtils.java
+++ src/main/java/com/carpool/carpool/util/JwtUtils.java
@@ -1,14 +1,11 @@
 package com.carpool.carpool.util;
 
-
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
@@ -22,7 +19,6 @@ public class JwtUtils {
 
     @Value("${security.jwt.refresh-time}")
     private long REFRESH_EXPIRATION;
-
 
     private SecretKey getKey() {
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
